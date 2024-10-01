78102a484c0fcd0abc27bb3ca12f7b5c63a5d982
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-01 17:31:56 +0000

    datatypes_summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,25 @@
+/*
+Primitive datatypes: 
+There are 7 of them - String, Number, Boolean, Null, Undefined, Symbol, BigInt
+*/ 
+
+/* Reference type or non primitive type
+Arrays, Objects, Functions
+*/
+
+// Javascript is a dynamically typed language.
+
+const heroes = ["Shaktimaan", "Naagraj", "HeroKiShakti"];
+let myObj = { 
+    name: "Mayank", 
+    age: 22
+}
+console.log(typeof myObj);
+
+const myFunction = function(){ 
+    console.log("Hello World");
+    
+}
+
+// Further to be read on ECMA Script Docs
+
