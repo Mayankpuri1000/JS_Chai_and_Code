7f68cc83f1c71d86fbb64367a016c2f468c2ea2d
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-01 14:52:58 +0000

    operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -11,6 +11,25 @@ let booleanLoggedIn = Boolean(loggedIn)
 // console.log(booleanLoggedIn);
 // console.log(typeof booleanLoggedIn);
 
+// *****************************************. Operations  ***********************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Mayank"
+// console.log(str1 + str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 1 + "2");
+
+// For prefix and postfix operator, refer to MDN Docs.
+
+
+
 
 
 
