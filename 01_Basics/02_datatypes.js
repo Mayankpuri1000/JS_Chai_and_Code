9259bd6e5d24345f7e1baaa1e9046d53c2953193
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-01 14:07:17 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+4) --> Not valid in Node.js, only runs in browser.
+
+let name = "Mayank" // String
+let age = 18 // Integer
+let isLoggedin = false //Boolean
+
+// Datatypes:-
+// number - 2^53 - 1
+// bigint
+// String
+// Null - A standalone value
+// Undefined - Not assigned a value
+// symbol - used for uniqueness
+
+// Object
+
+console.log(typeof "Mayank");
