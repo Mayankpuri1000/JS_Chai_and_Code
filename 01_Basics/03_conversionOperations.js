aab6f91dd55e2a5afb02ca1f17af29c0b808e2f4
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-01 14:36:44 +0000

    ConversionOperations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,16 @@
+// let score = "33abc"
+let score = null
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber));
+
+let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+// console.log(booleanLoggedIn);
+// console.log(typeof booleanLoggedIn);
+
+
+
+
