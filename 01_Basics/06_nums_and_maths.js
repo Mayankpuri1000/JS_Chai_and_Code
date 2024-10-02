5f358b37fad7dfd4dd033528bc05da93ffc0d4d5
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-02 14:43:11 +0000

    nums_and_maths

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -8,7 +8,32 @@ const balance = new Number(100)
 // console.log(balance.toFixed(3)); // Very useful while making e commerce applications
 
 const otherNumber = 123.12345
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// ********************.   MATHS.   *************************
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.33)); // Floor and Ceil methods are also used.
+// console.log(Math.max(4,5,6,7));
+// console.log(Math.min(4,5,6,7));
+
+// console.log(100*(Math.random())); // Used a lot when you are making dice games.
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min + 1) + min));
+
+
+
+
+
+
 
 
 
