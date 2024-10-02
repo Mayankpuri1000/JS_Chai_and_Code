5af9d7e342a8cb00f827cf99a7d44adf1ee08ecc
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-02 13:37:15 +0000

    nums_and_maths

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3)); // Very useful while making e commerce applications
+
+const otherNumber = 123.12345
+console.log(otherNumber.toPrecision(3));
+
+
+
+
