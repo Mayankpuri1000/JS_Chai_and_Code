08580e996219ff81ba382bf7a8a43bed6b0f0026
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-04 07:30:25 +0000

    date_and_time

---
+++ 01_Basics/07_date_and_time.js
@@ -0,0 +1,19 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+let myCreatedDate = new Date(2024, 11, 7);
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+
