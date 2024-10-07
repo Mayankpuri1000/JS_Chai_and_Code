0af47e838215814b182dbe06b732ce7ea4910605
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-07 13:48:14 +0000

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+// if statement
+
+const temp = 78
+if (temp === 41) {
+    console.log("Temperature is less than 50");
+    
+} else {
+    console.log("Temperature is greater than 50");
+    
+}
+console.log("Executed");
