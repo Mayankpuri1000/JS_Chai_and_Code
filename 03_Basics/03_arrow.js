315af315f6cc91cbe87ccb5dcdb3ae60fa6736dc
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-07 05:11:03 +0000

    arrow_functions

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -4,9 +4,40 @@ const user = {
 
     welcomeMessage: function(){ 
         console.log(`${this.userName}, Welcome to the website.`);
+        // console.log(this);
+        
     }
 }
 
-user.welcomeMessage();
-user.userName = "Sam"
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.userName = "Sam"
+// user.welcomeMessage();
+// console.log(this);
+
+// function chai(){ 
+//     console.log(this);
+    
+// }
+// chai();
+
+// const chai = () => {  // Syntax of Arrow functions
+//     let username = "Mayank"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+// ************************************************************************************************************************
+/* Explicit Return
+const addTwo = (num1, num2) => { 
+    return num1 + num2
+}
+console.log(addTwo(3,4));
+*/
+
+// Implicit Return
+const addTwo = (num1, num2) =>  ( num1 + num2 )
+
+console.log(addTwo(3,4));
+
