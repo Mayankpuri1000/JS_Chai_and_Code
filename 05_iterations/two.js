288a14a773d44bb205027994cf3e9a460ad70c4b
Mayankpuri1000 <98157797+Mayankpuri1000@users.noreply.github.com>
2024-10-09 05:28:12 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let index = 0;
+
+// while (index <= 10) {
+   // console.log(`Value of index is ${index}`);
+     // index += 2;
+// }
+
+// let myArray = ["Flash", "Superman", "Batman"]
+
+// while(index < myArray.length){ 
+//     console.log(`Value of the Array is: ${myArray[index]}`);
+//     index++;
+// }
+
+let score = 1
+do {
+    console.log(`Value of score is ${score}`);
+    score++;
+} while (score <= 10);
